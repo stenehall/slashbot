ea4d97d34b90c9b74c63d9ee57ba977566b5798f
Johan Stenehall <stenehall@gmail.com>
2014-02-24 21:29:07 +0100

    Removing unneeded load function, this is now handled in igelkott

--- slashbot.js
+++ slashbot.js
@@ -7,21 +7,6 @@ var Slashbot = function Slashbot(config) {
 
 Slashbot.prototype = Object.create(Igelkott.prototype, {constructor: {value: Slashbot}});
 
-Slashbot.prototype.load = function load(pluginName) {
-  try {
-    Igelkott.prototype.load.call(this, pluginName, config);
-  }
-  catch (err) {
-    this.log(err);
-    try {
-      this.plugin.load(pluginName, config.plugins[pluginName], require('igelkott-'+pluginName).Plugin);
-    } catch (err)
-    {
-      this.log('No such plugin: '+pluginName);
-    }
-  }
-};
-
 var config = require('./config.json');
 
 var slashbot = new Slashbot(config);
